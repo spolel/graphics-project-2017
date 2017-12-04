e3b23eb181f0b407524eb5e56b8b990f1c5f5565
Davide Bucher <shughi@davides-mbp-2.mobile.usilu.net>
2017-12-04 15:26:49 +0100

    initial commit, a rotating rocket and a bee.

---
+++ project_code/CCanvas.h
@@ -0,0 +1,77 @@
+/************************************************************************/
+/* Guards                                                               */
+/************************************************************************/
+#ifndef CCANVAS_H
+#define CCANVAS_H
+
+#include <iostream>
+#include <QtOpenGL>
+#include <QGLWidget>
+#include <QTimer>
+
+#include "Base.h"
+#include "texture.hpp"
+
+#include "ObjModel.h"
+#include "PlyModel.h"
+
+using namespace std;
+
+/************************************************************************/
+/* Canvas to draw                                                       */
+/************************************************************************/
+class CCanvas : public QGLWidget
+{
+  Q_OBJECT
+
+public:
+    explicit CCanvas(QWidget *parent = 0) : QGLWidget(parent),tau(0),
+        textureRocket("../../../../project_code/train1.jpg"),
+        textureBee("../../../../project_code/white.png"),
+        modelRocket("../../../../project_code/rocket.obj"),
+        modelBee("../../../../project_code/boo.obj")
+    {
+        QTimer *timer = new QTimer(this);
+        connect(timer, SIGNAL(timeout()), this, SLOT(updateGL()));
+        timer->start(10);
+    }
+
+protected:
+    void initializeGL();
+    void resizeGL(int width, int height);
+    void paintGL();
+
+private:
+    double tau;
+    void lookAt(const GLdouble eyex,
+                const GLdouble eyey,
+                const GLdouble eyez,
+                const GLdouble centerx,
+                const GLdouble centery,
+                const GLdouble centerz,
+                const GLdouble upx,
+                const GLdouble upy,
+                const GLdouble upz);
+
+    void glPerspective(const GLdouble fovy,
+                       const GLdouble aspect,
+                       const GLdouble zNear,
+                       const GLdouble zFar);
+
+
+    enum View {
+        Perspective = 0,    // View the scene from a perspective (from above, from a side, or whatever)
+        Cockpit             // View the scene from the train cockpit (if you want, or whatever other view)
+    };
+
+    void setView(View _view);
+
+    // Models and textures
+    Texture textureRocket;
+    Texture textureBee;
+    // Model loaded from .obj format
+    ObjModel modelRocket;
+    ObjModel modelBee;
+};
+
+#endif 
